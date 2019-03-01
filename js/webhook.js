c81c683851c484355556777ebe495d2768e1f631
Dalton <daltonwkiner@gmail.com>
2019-03-01 11:50:49 -0500

    Fixed Styles, and typeform

--- js/webhook.js
+++ js/webhook.js
@@ -1,20 +1,23 @@
 function Help() {
+   
     var nameInput = /*document.getElementById("nameIn").value*/"Someone";
     /*if (nameInput == "") {
         nameInput = "Someone"
     }*/
+    var content = document.getElementById("hdresponse");
+    var contentSetting = content.style.display;
     var xhr = new XMLHttpRequest();
-    /*if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }*/
+    if (contentSetting == "none") {
+        content.style.display = "block";
+    }
+    else {
+        content.style.display = "initial"
+    }
    
     xhr.open("POST", "https://maker.ifttt.com/trigger/callHelp/with/key/ZB_WQg5fBbfqAIZAZIxdE3cA8JGkS04XwBoC0ya97d?value1=" + nameInput, true);
     xhr.setRequestHeader("Content-type", "application/json");
     var data = JSON.stringify({ "value1": nameInput });
 
     xhr.send(data);
-    document.getElementById("response").innerHTML = "Someone will be with you shortly";
  
 }
