342202bd8f23ef3b7717e647b045a18d7dc7429c
Dalton <daltonwkiner@gmail.com>
2019-03-04 12:12:06 -0500

    Non IFTTT, using Slack bot now and fetchapi

--- js/webhook.js
+++ js/webhook.js
@@ -14,10 +14,15 @@ function Help() {
         content.style.display = "initial"
     }
    
-    xhr.open("POST", "https://maker.ifttt.com/trigger/callHelp/with/key/ZB_WQg5fBbfqAIZAZIxdE3cA8JGkS04XwBoC0ya97d?value1=" + nameInput, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    var data = JSON.stringify({ "value1": nameInput });
+    //xhr.open("POST", "https://maker.ifttt.com/trigger/callHelp/with/key/ZB_WQg5fBbfqAIZAZIxdE3cA8JGkS04XwBoC0ya97d?value1=" + nameInput, true);
+    fetch('https://hooks.slack.com/services/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', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc
+        body: JSON.stringify({ 'text': 'Someone is calling for help!'})
+    })
+    /*xhr.setRequestHeader("Content-type", "application/json");
+    
 
-    xhr.send(data);
+    xhr.send("text=test");
+    */
     setTimeout(function () { window.location.reload(false);}, 60000)
 }
