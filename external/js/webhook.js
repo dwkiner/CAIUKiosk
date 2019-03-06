2fabf96bf9225a88fa5281c3aa0529722779c096
Tech Intern <techintern1@caiu.org>
2019-03-06 11:04:14 -0500

    Fixed kinda

--- external/js/webhook.js
+++ external/js/webhook.js
@@ -1,1 +1,23 @@
-function Help() { var e = localStorage.getItem("roomName"); document.getElementById("hdresponse").style.display, document.getElementById("hdresponse").style.display = "block", $.ajax({ data: "payload=" + JSON.stringify({ text: "Someone is calling for help from the " + e + " room!" }), dataType: "json", processData: !1, type: "POST", url: "https://hooks.slack.com/services/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" }), setTimeout(function () { window.location.reload(!1) }, 3e4) }
+function Help() {
+   
+    var roomName = localStorage.getItem("roomName");
+   
+    
+        document.getElementById("hdresponse").style.display = "block";
+        document.getElementById("hdresponse1").style.display = "none";
+    
+    $.ajax({
+        data: 'payload=' + JSON.stringify({
+            "text": "Someone is calling for help from the " + roomName + " room!"
+        }),
+        dataType: 'json',
+        processData: false,
+        type: 'POST',
+        url: "https://hooks.slack.com/services/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"
+
+    });
+    
+   
+   
+    setTimeout(function () { window.location.reload(false);}, 30000)
+}
