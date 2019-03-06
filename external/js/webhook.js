4ffca856c545857c4a635639ea8e4819dcc3e9ea
Tech Intern <techintern1@caiu.org>
2019-03-06 10:17:34 -0500

    Trying to make things less meaty

--- external/js/webhook.js
+++ external/js/webhook.js
@@ -1,22 +1,16 @@
 function Help() {
-   //document.getElementById("buttons").style.display = "none"
+   
     var roomName = localStorage.getItem("roomName");
-    var nameInput = /*document.getElementById("nameIn").value*/"Someone";
-    if (nameInput == "") {
-        nameInput = "Someone"
-    }
-    var content = document.getElementById("hdresponse");
-    var contentSetting = content.style.display;
-    var xhr = new XMLHttpRequest();
-   /* if (contentSetting == "none") {
-        content.style.display = "block";
+   
+    if (document.getElementById("hdresponse").style.display == "none") {
+        document.getElementById("hdresponse").style.display = "block";
     }
     else {
-        content.style.display = "block"
-    }*/
+        document.getElementById("hdresponse").style.display = "block";
+    }
     $.ajax({
         data: 'payload=' + JSON.stringify({
-            "text": nameInput + " is calling for help from the " + roomName + " room!"
+            "text": "Someone is calling for help from the " + roomName + " room!"
         }),
         dataType: 'json',
         processData: false,
