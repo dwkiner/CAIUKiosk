8fe6697ca0c055c7d4129447453e5c2611a7a44e
Dalton Kiner <daltonwkiner@gmail.com>
2021-07-05 11:10:55 -0400

    Update webhook.js

--- js/webhook.js
+++ js/webhook.js
@@ -11,7 +11,7 @@ function Help() {
         x.style.display = "none";
     }*/
     //https://maker.ifttt.com/trigger/callHelp/with/key/ZB_WQg5fBbfqAIZAZIxdE3cA8JGkS04XwBoC0ya97d
-    xhr.open("GET", "https://webhook.site/c7761c59-f678-40b6-b68a-d651d251561d", true);
+    xhr.open("GET", "INSERT WEBHOOK SITE HERE FOR SLACK", true);
    /* xhr.setRequestHeader("Content-type", "application/json");
     var data = JSON.stringify({ "value1": nameInput });
 
